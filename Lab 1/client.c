eac1edf2ba86bbd5223c0107c5691404e2c8998a
Allen Jiang <alljiang@hotmail.com>
2023-08-27 14:45:31 -0500

    add lab 1

---
+++ Lab 1/client.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Lab 1/server.c
@@ -0,0 +1,11 @@
+#include <netinet/in.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+int main() {
+  printf("Hello, World 2!\n");
+  return 0;
+}
